fe851c9ce164c44a98ac951f0a772e096b6a8047
victorgh <victor.gonzalez@soprasteria.com>
2020-03-05 11:38:34 +0100

    feat(state-example): useState() basic example

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import StateExample from './components/state-hook.js';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+      <StateExample />
       </header>
     </div>
   );
