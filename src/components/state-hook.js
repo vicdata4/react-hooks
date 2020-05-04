8c6854386b89557ef23dfaca3d3fd42fc756ec9a
vicdata <vicdata@outlook.com>
2020-05-04 16:00:10 +0200

    State-hook getCurrentTime()

--- src/components/state-hook.js
+++ src/components/state-hook.js
@@ -4,9 +4,14 @@ import StateHookChild from './state-hook-child.js';
 function StateHook() {
   // Declara una nueva variable de estado, que llamaremos "count".
   const [count, setCount] = useState(0);
+  const [currentTime, setCurrentTime] = useState(Date.now());
 
   function onChildSubmit(e) {
     console.log(`My child sent me: ${e}`);
+  }
+
+  function getCurrentTime() {
+    return `${new Date(currentTime)}`;
   }
 
   return (
@@ -15,6 +20,10 @@ function StateHook() {
       <button onClick={() => setCount(count + 1)}>
         Click me
       </button>
+      <button onClick={() => setCurrentTime(Date.now())}>
+        Update current time
+      </button>
+      <p>Current time: {getCurrentTime()}</p>
       <StateHookChild count={count} onChildSubmit={onChildSubmit} />
     </div>
   );
